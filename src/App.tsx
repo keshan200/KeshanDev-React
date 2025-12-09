1d464555fced577fb241f263900df21bc351a23e
keshan200 <sachithakeshan175@gmail.com>
2025-12-09 09:35:59 +0530

    taiwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <main id="About-page" className="w-full bg-black py-12 px-4">
+      <section className="flex flex-col md:flex-row justify-evenly items-center gap-10 md:gap-10">
+       
+        {/* Image Section */}
+        <div data-aos="zoom-in-up">
+          <img
+            
+            alt="Keshan"
+            className="w-[450px] rounded-2xl max-w-full"
+          />
+        </div>
+
+        {/* Text Section */}
+        <div
+          data-aos="zoom-in-up"
+          className="flex flex-col items-start justify-center w-full md:w-1/2 text-left"
+        >
+          {/* Title */}
+          <h3
+            id="first"
+            className="text-(--accent-color) font-bold  text-lg flex items-center"
+          >
+            <span className="text-xl text-(--accent-color) px-2">&#9733;</span>
+            MY STORY
+            <span className="text-xl text-(--accent-color) px-2">&#9733;</span>
+          </h3>
+
+          {/* Subtitle */}
+          <h2
+            id="sec"
+           className="text-white font-bold text-3xl sm:text-8xl lg:text-5xl mt-3"
+
+          >
+            Passionate Developer Crafting Solutions for the Future
+          </h2>
+
+          {/* Description */}
+          <p
+            id="third"
+            className="text-gray-300  text-base sm:text-md mt-5 leading-relaxed"
+          >
+            I am passionate about using technology to solve complex problems and
+            make a positive impact on the world. So currently I am studying
+            Software Engineering at IJSE - Institute of Software Engineering.
+            As a creative problem solver and team player, I am committed to
+            delivering high-quality work and exceeding expectations. I am eager
+            to connect with other technology professionals and explore new
+            opportunities for growth and development.
+            <br />
+          </p>
+
+          {/* Button */}
+          <button
+            data-aos="zoom-in-up"
+            className="mt-8 px-5 py-2 bg-(--accent-color) text-black font-bold rounded-full  hover:scale-105 transition-transform duration-300"
+          >
+            Send Message
+          </button>
+        </div>
+      </section>
+    </main>
+        
     </>
   )
 }
